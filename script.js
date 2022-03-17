a3c36e12d4738df17d88a634fa335568329b188d
Meercat33 <75919686+Meercat33@users.noreply.github.com>
2022-03-16 22:43:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,59 +3,36 @@ import * as CANNON from 'cannon-es';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 //import {PointerLockControls} from 'three/examples/jsm/controls/PointerLockControls.js'; <- Optional
 
-let scene, renderer, camera, world;
-const timeStep = 1 / 60;
 const aspectRatio /*bozo*/ = window.innerWidth / window.innerHeight;
 
+//init CANNON
+const timeStep = 1 / 60;
+const world = new CANNON.World({gravity: new CANNON.Vec3(0, -9.81, 0)});
 
-function main() {
-    initCannon();
-    initThree();
-    const material = new THREE.MeshNormalMaterial();
+//init THREE
+const scene = new THREE.Scene();
 
-    const boxGeo = new THREE.BoxGeometry(7, 7, 7);
-    const boxMsh = new THREE.Mesh(boxGeo, material);
-    scene.add(boxMsh);
+const camera = new THREE.PerspectiveCamera(75, aspectRatio);
+camera.setPosition(0, 10, 30)
+camera.lookAt(0, 0, 0)
 
-    const light = new THREE.AmbientLight(0xffffff, 0.7);
-    scene.add(light);
-}
+const renderer = new THREE.WebGL1Renderer({antialias:true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-function initThree() {
-    scene = new THREE.Scene();
+const controls = new OrbitControls(camera, renderer.domElement);
 
-    renderer = new THREE.WebGL1Renderer({antialias:true});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    camera = new THREE.PerspectiveCamera(75, aspectRatio);
-    camera.position.set(0, 10, 30);
-    camera.lookAt(0, 0, 0);
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-
-    animate();
-
-    window.addEventListener('resize', onWindowResize, false);
-}
-
-function initCannon() {
-    world = new CANNON.World({
-        gravity: new CANNON.Vec3(0, -9.81, 0)
-    });
-}
+animate();
+//end
 
 function animate() {
     requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    updatePhysics();
+}
+
+function updatePhysics() {
     world.step(timeStep);
 }
 
-function onWindowResize() {
-    camera.aspect = aspectRatio;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-main();
+//unfinished
